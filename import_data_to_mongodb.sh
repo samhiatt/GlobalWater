4a64b198da3b542354f1456e04ad775f03d0acea
Sam Hiatt <sam.hiatt@gmail.com>
2015-10-14 23:41:07 -0700

    Only call ogr2ogr if geojson not found.

--- import_data_to_mongodb.sh
+++ import_data_to_mongodb.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-ogr2ogr -f GeoJSON ne_50m_lakes/ne_50m_lakes.geojson ne_50m_lakes/ne_50m_lakes.shp;
-ogr2ogr -f GeoJSON ne_50m_ocean/ne_50m_ocean.geojson ne_50m_ocean/ne_50m_ocean.shp;
-
-echo "db.water.drop();" | mongo topo &&
-    jq --compact-output ".features" ne_50m_ocean/ne_50m_ocean.geojson | mongoimport -d topo -c water --type=json --jsonArray &&
-    jq --compact-output ".features" ne_50m_lakes/ne_50m_lakes.geojson | mongoimport -d topo -c water --type=json --jsonArray &&
-    (mongo topo init_mongo.mongo.js || echo "Error running init_mongo.mongo.js")
-
+[ -f ne_50m_lakes/ne_50m_lakes.geojson ] || ogr2ogr -f GeoJSON ne_50m_lakes/ne_50m_lakes.geojson ne_50m_lakes/ne_50m_lakes.shp &&
+[ -f ne_50m_ocean/ne_50m_ocean.geojson ] || ogr2ogr -f GeoJSON ne_50m_ocean/ne_50m_ocean.geojson ne_50m_ocean/ne_50m_ocean.shp &&
+echo "db.water.drop();" | mongo topo  &&
+jq --compact-output ".features" ne_50m_ocean/ne_50m_ocean.geojson | mongoimport -d topo -c water --type=json --jsonArray &&
+jq --compact-output ".features" ne_50m_lakes/ne_50m_lakes.geojson | mongoimport -d topo -c water --type=json --jsonArray &&
+(mongo topo init_mongo.mongo.js || echo "Error running init_mongo.mongo.js") \
+|| echo "Error importing data."
