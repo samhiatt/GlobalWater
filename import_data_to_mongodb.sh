daae33120120bc7a14e9ccb4f38812ea29c7a8c2
Sam Hiatt <sam.hiatt@gmail.com>
2015-10-19 23:21:21 -0700

    added command line args for specifying target db and collection

--- import_data_to_mongodb.sh
+++ import_data_to_mongodb.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
-
+HOST="localhost:27017"
+DB="topo"
+if [ ! -z "$1" ]
+  then
+    DB=$1
+    if [ ! -z "$2" ]; then
+      HOST=$2
+    fi;
+fi
+echo "HOST:$HOST DB:$DB"
 [ -f ne_50m_lakes/ne_50m_lakes.geojson ] || ogr2ogr -f GeoJSON ne_50m_lakes/ne_50m_lakes.geojson ne_50m_lakes/ne_50m_lakes.shp &&
 [ -f ne_50m_ocean/ne_50m_ocean.geojson ] || ogr2ogr -f GeoJSON ne_50m_ocean/ne_50m_ocean.geojson ne_50m_ocean/ne_50m_ocean.shp &&
-echo "db.water.drop();" | mongo topo  &&
-jq --compact-output ".features" ne_50m_ocean/ne_50m_ocean.geojson | mongoimport -d topo -c water --type=json --jsonArray &&
-jq --compact-output ".features" ne_50m_lakes/ne_50m_lakes.geojson | mongoimport -d topo -c water --type=json --jsonArray &&
-(mongo topo init_mongo.mongo.js || echo "Error running init_mongo.mongo.js") \
+echo "db.topo.water.drop();" | mongo $HOST/$DB  &&
+jq --compact-output ".features" ne_50m_ocean/ne_50m_ocean.geojson | mongoimport -d $DB -c topo.water --type=json --jsonArray &&
+jq --compact-output ".features" ne_50m_lakes/ne_50m_lakes.geojson | mongoimport -d $DB -c topo.water --type=json --jsonArray &&
+(mongo $HOST/$DB init_mongo.mongo.js || echo "Error running init_mongo.mongo.js") \
 || echo "Error importing data."
--- init_mongo.mongo.js
+++ init_mongo.mongo.js
@@ -1,10 +1,10 @@
 
 
-var ocean=db.water.findOne({'properties.featurecla':'Ocean'});
+var ocean=db.topo.water.findOne({'properties.featurecla':'Ocean'});
 ocean.geometry.coordinates.forEach(function(polygon){
     polygon.forEach(function(coords){
-        db.water.insert({"type" : "Feature","properties" : {"featurecla" : "Ocean"},"geometry":{"type":"MultiLineString","coordinates":[coords]}});
+        db.topo.water.insert({"type" : "Feature","properties" : {"featurecla" : "Ocean"},"geometry":{"type":"MultiLineString","coordinates":[coords]}});
     });
 });
-db.water.remove({'properties.featurecla':'Ocean','geometry.type':"MultiPolygon"});
-db.water.ensureIndex({geometry:'2dsphere'});
+db.topo.water.remove({'properties.featurecla':'Ocean','geometry.type':"MultiPolygon"});
+db.topo.water.ensureIndex({geometry:'2dsphere'});
